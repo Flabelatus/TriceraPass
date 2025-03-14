3858492fa5bc0051329edd53a32fc88675da8992
Flabelatus <jooshesh.javid@gmail.com>
2025-03-14 20:06:15 +0100

    fix(middleware): Revert to development config

--- cmd/api/application/middleware.go
+++ cmd/api/application/middleware.go
@@ -43,14 +43,13 @@ func (app *Application) EnableCORS(h http.Handler) http.Handler {
 		origin := r.Header.Get("Origin")
 		if isOriginAllowed(origin, allowOriginList) {
 			for _, url := range allowOriginList {
-				fmt.Println(url)
 				w.Header().Set("Access-Control-Allow-Origin", url)
-				w.Header().Set("Access-Control-Allow-Credentials", "true")
+				// w.Header().Set("Access-Control-Allow-Credentials", "true")
 			}
 		}
 
 		if r.Method == "OPTIONS" {
-			// w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
 			w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS")
 			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, X-Auth-Email, X-Auth-Key, X-CSRF-Token, Origin, X-Requested-With, Authorization")
 			return
